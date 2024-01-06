ab242e7b690043f2384edb5ed7bcc844e41b4a15
august49 <augustiniusngongo@gmail.com>
2024-01-07 04:03:35 +0530

    auth middleware

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const { createNewUser, verifyEmail } = require("../handlers/user");
 const asyncMiddleware = require("../middleware/asyncMiddleware");
 const express = require("express");
-
+const rateLimit = require("express-rate-limit");
 const router = express.Router();
+const limit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  message: "Too many request from this IP , please try again after 15 minutes",
+});
 
-router.post("/newUser", asyncMiddleware(createNewUser));
-router.get("/verifyEmail/:token", asyncMiddleware(verifyEmail));
+router.post("/newUser", limit, asyncMiddleware(createNewUser));
+router.get("/verifyEmail/:token", limit, asyncMiddleware(verifyEmail));
 
 module.exports = router;
